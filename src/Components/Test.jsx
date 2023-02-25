cfd9589a9a36b4ea01004ce28f83ce90cf768ef8
Paul Spencer <billsongames@gmail.com>
2023-02-25 15:53:53 +0000

    first commit

---
+++ src/Components/Test.jsx
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from "react";
+
+import { testData } from "../data/data";
+
+import axios from 'axios'
+
+
+//title
+//author
+//source/domain
+//image
+//content
+//url
+//topics
+//categories
+
+function Test() {
+
+  const [articles,setArticles] = useState([])
+
+  const searchString = "Manchester United"
+  
+  useEffect(() => {
+    axios
+      .get(`https://gnews.io/api/v4/top-headlines?q=${searchString}&country=gb&category=sport&sortby=publishedAt&apikey=445b4b502608f3804329f4428b41b723`)
+
+      .then(function (response) {
+        setArticles(response.data.articles)
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+    }, [])    
+
+
+
+
+
+
+  return(
+    <div>
+    <h2>Articles</h2>
+    <div>
+      {articles.map((article) => (
+        <div key={article.title}>
+          <div>
+            Title = {article.title}
+          </div>
+
+          <div>
+            Description = {article.description}
+          </div>
+
+          <div>
+            Content = {article.content}
+          </div>
+
+          <div>
+            <a href={article.url}>Full article</a>
+          </div>
+
+          <div>
+            <img src={article.image} width={200}></img>
+          </div>
+
+          <div>
+            Source = <a href={article.source.url}>{article.source.name}</a>
+          </div>
+      </div>
+      ))}
+    </div>  
+  </div>
+  )
+}
+
+export default Test
---
+++ src/data/data.js
@@ -0,0 +1,126 @@
+export const testData = 
+{
+  "totalArticles": 7196,
+  "articles": [
+      {
+          "title": "Peter Schmeichel identifies Man Utd's \"odd one out\" as he criticises transfer policy",
+          "description": "Peter Schmeichel launched a scathing attack on Manchester United's transfer business during the January window after they fell behind against Barcelona on Thursday night",
+          "content": "Peter Schmeichel launched a scathing attack on Manchester United's transfer business during the January window after they fell behind against Barcelona on Thursday night\nPeter Schmeichel believes Erik ten Hag should have given an opportunity to a you... [3126 chars]",
+          "url": "https://www.mirror.co.uk/sport/football/news/manutd-transfers-barcelona-tenhag-weghorst-29307406",
+          "image": "https://i2-prod.mirror.co.uk/sport/article29307731.ece/ALTERNATES/s1200/0_Screenshot-2023-02-24-183152.jpg",
+          "publishedAt": "2023-02-24T19:06:09Z",
+          "source": {
+              "name": "The Mirror",
+              "url": "https://www.mirror.co.uk"
+          }
+      },
+      {
+          "title": "Man United fans shower 'top guy' Lisandro Martinez with praise after he stayed to sign autographs",
+          "description": "United progressed to the last 16 of the Europa League with 4-3 aggregate win over Barcelona at Old Trafford. Martinez shone in the game at centre-back as Antony scored the winner.",
+          "content": "Lisandro Martinez delighted Manchester United fans by staying late to sign autographs on Thursday.\nUnited progressed to the last 16 of the Europa League with 4-3 aggregate win over Barcelona at Old Trafford.\nMartinez shone in the game at centre-back ... [1805 chars]",
+          "url": "https://www.dailymail.co.uk/sport/football/article-11789305/Man-United-fans-shower-guy-Lisandro-Martinez-praise-stayed-sign-autographs.html",
+          "image": "https://i.dailymail.co.uk/1s/2023/02/24/13/68040871-0-image-a-72_1677245696849.jpg",
+          "publishedAt": "2023-02-24T13:53:46Z",
+          "source": {
+              "name": "Daily Mail",
+              "url": "https://www.dailymail.co.uk"
+          }
+      },
+      {
+          "title": "Alejandro Garnacho is SLAMMED by Spanish media for his 'very poor taste' after mocking Pedri",
+          "description": "The 18-year-old was a second half substitute in the match at Old Trafford on Thursday in a dramatic game that saw the Red Devils dump the Spanish giants out of the Europa League.",
+          "content": "Alejandro Garnacho has been slammed by the Spanish press following Manchester United's game against Barcelona.\nThe 18-year-old was a second half substitute in the match at Old Trafford on Thursday in a dramatic game that saw the Red Devils dump the S... [1739 chars]",
+          "url": "https://www.dailymail.co.uk/sport/football/article-11789147/Alejandro-Garnacho-SLAMMED-Spanish-media-poor-taste-mocking-Pedri.html",
+          "image": "https://i.dailymail.co.uk/1s/2023/02/24/13/68040175-0-image-a-60_1677243747363.jpg",
+          "publishedAt": "2023-02-24T12:42:49Z",
+          "source": {
+              "name": "Daily Mail",
+              "url": "https://www.dailymail.co.uk"
+          }
+      },
+      {
+          "title": "Europa League draw, League Cup finals and Sellés in at Saints: football news - live",
+          "description": "Counting down to the weekend of football action. Join our writers for the latest news",
+          "content": "11m ago 06.25 EST The Europa League Round of 16 draw, in which Arsenal have been paired with Sporting and Manchester United with Real Betis. Photograph: Denis Balibouse/Reuters\n14m ago 06.22 EST Europa League Round of 16 draw FC Union Saint-Gilloise ... [5715 chars]",
+          "url": "https://www.theguardian.com/football/live/2023/feb/24/league-cup-finals-europa-league-draw-and-selles-in-at-saints-football-news-live",
+          "image": "https://i.guim.co.uk/img/media/a9ab5a57ffd7b108899e8ae9dcb3466d5c3e80f2/0_218_6534_3920/master/6534.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctbGl2ZS5wbmc&enable=upscale&s=5471925e040078c8c85ec256af9c203e",
+          "publishedAt": "2023-02-24T11:25:41Z",
+          "source": {
+              "name": "The Guardian",
+              "url": "https://www.theguardian.com"
+          }
+      },
+      {
+          "title": "Europa League fixtures: Man Utd vs Real Betis, Arsenal vs Sporting Lisbon",
+          "description": "Manchester United will face more Spanish opposition in the form of Real Betis in the Europa League last-16, while Arsenal have been paired with Sporting Lisbon.",
+          "content": "Manchester United will face more Spanish opposition in the form of Real Betis in the Europa League last-16, while Arsenal have been paired with Sporting Lisbon.\nErik ten Hag's side overcame Barcelona 4-3 on aggregate in a thrilling play-off round tie... [3184 chars]",
+          "url": "https://www.dailymail.co.uk/sport/football/article-11788829/Europa-League-draw-Manchester-United-play-Real-Betis-Arsenal-Sporting-Lisbon.html",
+          "image": "https://i.dailymail.co.uk/1s/2023/02/24/11/68036921-0-image-a-27_1677238515090.jpg",
+          "publishedAt": "2023-02-24T11:22:50Z",
+          "source": {
+              "name": "Daily Mail",
+              "url": "https://www.dailymail.co.uk"
+          }
+      },
+      {
+          "title": "Erik ten Hag reveals his half-time team talk that inspired comeback victory over Barcelona",
+          "description": "Robert Lewandowski gave the Spanish giants the lead in the first-half before Fred and Antony both got themselves on the scoresheet in the second-half to send United through to the last-16.",
+          "content": "Manchester United boss Erik ten Hag has revealed that he urged his players to believe in themselves at half-time of their 2-1 comeback victory over Barcelona in the Europa League.\nRobert Lewandowski gave the Spanish giants the lead in the first-half ... [2603 chars]",
+          "url": "https://www.dailymail.co.uk/sport/football/article-11788211/Erik-ten-Hag-reveals-half-time-team-talk-inspired-comeback-victory-Barcelona.html",
+          "image": "https://i.dailymail.co.uk/1s/2023/02/24/08/68031335-0-image-a-18_1677225806454.jpg",
+          "publishedAt": "2023-02-24T08:14:14Z",
+          "source": {
+              "name": "Daily Mail",
+              "url": "https://www.dailymail.co.uk"
+          }
+      },
+      {
+          "title": "Rio Ferdinand picks out Man Utd's two most important players from Barcelona win",
+          "description": "Former Manchester United defender Rio Ferdinand was full of praise for Antony and Fred after they both scored in the crucial Europa League play-off match against Barcelona",
+          "content": "Former Manchester United defender Rio Ferdinand was full of praise for Antony and Fred after they both scored in the crucial Europa League play-off match against Barcelona\nRio Ferdinand has hailed Antony and Fred after the Brazilian duo both scored f... [2651 chars]",
+          "url": "https://www.mirror.co.uk/sport/football/news/man-utd-rio-ferdinand-barcelona-29300088",
+          "image": "https://i2-prod.mirror.co.uk/incoming/article29269776.ece/ALTERNATES/s1200/0_Screenshot-2361.jpg",
+          "publishedAt": "2023-02-24T06:40:38Z",
+          "source": {
+              "name": "The Mirror",
+              "url": "https://www.mirror.co.uk"
+          }
+      },
+      {
+          "title": "Eddie Howe reveals Kevin Keegan plan Newcastle will 'definitely arrange' even if 'difficult'",
+          "description": "Newcastle United head coach Eddie Howe reached out to Kevin Keegan before he even took the job and the Magpies boss will be inviting the club legend to speak to his players",
+          "content": "Eddie Howe has vowed to 'definitely' arrange for Kevin Keegan to come in to speak to his Newcastle United players - even if it is not possible before the Carabao Cup final.\nKeegan was one of the first people Howe phoned before the Newcastle boss form... [1783 chars]",
+          "url": "https://www.chroniclelive.co.uk/sport/football/football-news/newcastle-eddie-howe-kevin-keegan-26313084",
+          "image": "https://i2-prod.chroniclelive.co.uk/sport/football/football-news/article26314040.ece/ALTERNATES/s1200/0_Untitled-design.png",
+          "publishedAt": "2023-02-24T06:00:00Z",
+          "source": {
+              "name": "Chronicle Live",
+              "url": "https://www.chroniclelive.co.uk"
+          }
+      },
+      {
+          "title": "Clinical finish from Antony takes Manchester United past Barcelona",
+          "description": "Second-half goals from Antony and Fred helped United beat Barcelona 2-1 to take them into the last 16 of the Europa League",
+          "content": "It is probably fair to say that the jury has been out on Antony since Manchester United’s decision to pay €100m for him last summer. The most insistent criticism has been that he is too predictable. Defenders know that he wants to come in from the ri... [5726 chars]",
+          "url": "https://www.theguardian.com/football/2023/feb/23/clinical-finish-from-antony-takes-manchester-united-past-barcelona",
+          "image": "https://i.guim.co.uk/img/media/82b09bb4197888f88edbf2e717f97e1f4f471af3/0_228_4640_2784/master/4640.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=f660a0caf6530cf8d4d896f940af627c",
+          "publishedAt": "2023-02-23T21:59:00Z",
+          "source": {
+              "name": "The Guardian",
+              "url": "https://www.theguardian.com"
+          }
+      },
+      {
+          "title": "Bruno Fernandes criticised after 'stupid' decision gifts Barcelona a penalty against Man United",
+          "description": "Former Manchester United goalkeeper Peter Schmeichel has criticised Bruno Fernandes for a stupid decision after the midfielder gifted Barcelona a penalty in their Europa League tie.",
+          "content": "Former Manchester United goalkeeper Peter Schmeichel has criticised Bruno Fernandes for a stupid decision after the midfielder gifted Barcelona a penalty in their Europa League tie.\nFernandes was adjudged to have pulled back Alejandro Balde in the pe... [1594 chars]",
+          "url": "https://www.dailymail.co.uk/sport/football/article-11786427/Bruno-Fernandes-criticised-stupid-decision-gifts-Barcelona-penalty-against-Man-United.html",
+          "image": "https://i.dailymail.co.uk/1s/2023/02/23/21/68016619-0-image-a-38_1677187438310.jpg",
+          "publishedAt": "2023-02-23T20:49:08Z",
+          "source": {
+              "name": "Daily Mail",
+              "url": "https://www.dailymail.co.uk"
+          }
+      }
+  ]
+}
