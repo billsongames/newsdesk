71c3e182483dce5e67203d7579d503dbf796bc59
Carl Beckley <carlbeckley@gmail.com>
2023-02-26 11:34:56 +0000

    Navbar created

---
+++ src/Components/Test/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = () => {
+    <div className="navbar-container">
+        <img className="navbar-logo" src="/" alt="logo" />
+        <ul className="navbar-categories">
+            <li className="navbar-categories-item">Tech</li>
+            <li className="navbar-categories-item">Business</li>
+            <li className="navbar-categories-item">Politics</li>
+            <li className="navbar-categories-item">Sport</li>
+            <li className="navbar-categories-item">Entertainment</li>
+        </ul>
+        
+
+    </div>
+}
+
+export default NavBar;
