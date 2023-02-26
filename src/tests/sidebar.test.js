59ded74d9939f8adbd1ea1136dd122653e3edd32
Carl Beckley <carlbeckley@gmail.com>
2023-02-26 20:05:11 +0000

    added tests for sidebar

--- src/tests/sidebar.test.js
+++ src/tests/sidebar.test.js
@@ -1,3 +1,23 @@
 import React from "react";
+import { render, screen } from "@testing-library/react";
 
 import Sidebar from "../Components/Test/Sidebar"
+
+describe("Sidebar", () => {
+    it("renders filters text and options", () => {
+        render(<Sidebar />);
+
+        expect(screen.getByText("Filters")).toBeInTheDocument();
+        expect(screen.getByText("Most Recent")).toBeInTheDocument();
+        expect(screen.getByText("By Publisher")).toBeInTheDocument();
+    });
+
+    it("renders share text and links", () => {
+        render(<Sidebar />);
+
+        expect(screen.getByText("Share")).toBeInTheDocument();
+        expect(screen.getByText("Email")).toBeInTheDocument();
+        expect(screen.getByText("Twitter")).toBeInTheDocument();
+        expect(screen.getByText("Facebook")).toBeInTheDocument();
+    });
+});
