fd3ac0abc8978eadf60e2a96fb24572b70963607
Carl Beckley <carlbeckley@gmail.com>
2023-02-26 20:07:59 +0000

    added initial options to sidebar component

--- src/Components/Test/Sidebar.js
+++ src/Components/Test/Sidebar.js
@@ -4,10 +4,22 @@ import "../../styles/sidebar.css"
 
 const Sidebar = () => {
 
-    return (
-        <div>
-            
-        </div>
-    )
+return (
+    <div className="sidebar-container">
+    <h2 className="sidebar-filters-text">Filters</h2>
+    <ul className="sidebar-filters">
+        <li className="sidebar-filters-item"><a href="#">Most Recent</a></li>
+        <li className="sidebar-filters-item"><a href="#">By Publisher</a></li>
+    </ul>
 
+    <h2 className="sidebar-share-text">Share</h2>
+    <ul className="sidebar-share">
+        <li className="sidebar-share-item"><a href="#">Email</a></li>
+        <li className="sidebar-share-item"><a href="#">Twitter</a></li>
+        <li className="sidebar-share-item"><a href="#">Facebook</a></li>
+    </ul>
+    </div>
+);
 };
+
+export default Sidebar;
