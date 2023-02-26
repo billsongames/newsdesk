ad0d5421809e7305e3f706a7346ea7d9259bd2d7
Carl Beckley <carlbeckley@gmail.com>
2023-02-26 11:40:10 +0000

    added input box and button

--- src/Components/Test/Navbar.js
+++ src/Components/Test/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "../../styles/navbar.css"
+
 const NavBar = () => {
     <div className="navbar-container">
         <img className="navbar-logo" src="/" alt="logo" />
@@ -10,6 +12,18 @@ const NavBar = () => {
             <li className="navbar-categories-item">Sport</li>
             <li className="navbar-categories-item">Entertainment</li>
         </ul>
+
+        <form className='search-form'>
+            <input 
+                className='search-form__input-box' 
+                type='text' 
+                placeholder='Search' 
+        />
+    
+        <button className='search-form__button' type='submit' data-testid='button'>
+            Search
+        </button>
+        </form>
         
 
     </div>
