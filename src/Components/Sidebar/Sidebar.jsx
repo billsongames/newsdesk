dafe6edc24f572a26dcd7686bd18896cceb35b97
Carl Beckley <carlbeckley@gmail.com>
2023-02-28 15:47:33 +0000

    added initial functionality to sort by date

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,22 +1,37 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import qs from "qs";
 
 import "./sidebar.css"
 
 const Sidebar = () => {
+    const { search } = useLocation();
+
+    const buildQueryString = (operation, valueObj) => {
+        const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+        const newQueryParams = {
+            ...currentQueryParams,
+            [operation]: JSON.stringify(valueObj),
+        };
+        return qs.stringify(newQueryParams, {
+            addQueryPrefix: true,
+            encode: false,
+        });
+    };
 
 return (
     <div className="sidebar-container">
-    <h2 className="sidebar-filters-text">Filters</h2>
+    <h2 className="sidebar-filters-text">Sort by Date</h2>
     <ul className="sidebar-filters">
-        <li className="sidebar-filters-item"><a href="#">Most Recent</a></li>
-        <li className="sidebar-filters-item"><a href="#">By Publisher</a></li>
+        <li className="sidebar-filters-item"><Link to={buildQueryString("sort", { publishedAt: -1 })}>Most Recent</Link></li>
+        <li className="sidebar-filters-item"><Link to={buildQueryString("sort", { publishedAt: 1 })}>Oldest</Link></li>
     </ul>
 
     <h2 className="sidebar-share-text">Share</h2>
     <ul className="sidebar-share">
-        <li className="sidebar-share-item"><a href="#">Email</a></li>
-        <li className="sidebar-share-item"><a href="#">Twitter</a></li>
-        <li className="sidebar-share-item"><a href="#">Facebook</a></li>
+        <li className="sidebar-share-item"><a href="/">Email</a></li>
+        <li className="sidebar-share-item"><a href="/">Twitter</a></li>
+        <li className="sidebar-share-item"><a href="/">Facebook</a></li>
     </ul>
     </div>
 );
