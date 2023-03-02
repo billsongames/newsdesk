854a50bd071f29c95b50265c46967590562d572f
Carl Beckley <carlbeckley@gmail.com>
2023-03-02 20:02:52 +0000

    formatting changes

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -3,48 +3,48 @@ import React, { useState } from "react";
 import "./sidebar.css"
 
 const Sidebar = (props) => {
-    const { reverseOrder, articles, selected, setSelected } = props;
-    const [newArticles, setNewArticles] = useState(true);
-    const [oldArticles, setOldArticles] = useState(false);
+  const { reverseOrder, articles, selected, setSelected } = props;
+  const [newArticles, setNewArticles] = useState(true);
+  const [oldArticles, setOldArticles] = useState(false);
 
-    const newClickHandler = () => {
-        setNewArticles(true);
-        setOldArticles(false);
-        reverseOrder();
-};
+  const newClickHandler = () => {
+    setNewArticles(true);
+    setOldArticles(false);
+    reverseOrder();
+  };
 
-    const oldClickHandler = () => {
-        setNewArticles(false);
-        setOldArticles(true);
-        reverseOrder();
-    };
+  const oldClickHandler = () => {
+    setNewArticles(false);
+    setOldArticles(true);
+    reverseOrder();
+  };
 
 return (
-    <div className="sidebar-container">
-        <h2 className="sidebar-filters-text">Sort by Date</h2>
-        <div className="sidebar">
-            <button className="sidebar-button" onClick={newClickHandler} disabled={newArticles}>Most Recent</button>
-            <button className="sidebar-button" onClick={oldClickHandler} disabled={oldArticles}>Oldest</button>
-        </div>
+  <div className="sidebar-container">
+    <h2 className="sidebar-filters-text">Sort by Date</h2>
+      <div className="sidebar">
+        <button className="sidebar-button" onClick={newClickHandler} disabled={newArticles}>Most Recent</button>
+        <button className="sidebar-button" onClick={oldClickHandler} disabled={oldArticles}>Oldest</button>
+      </div>
 
     <div>
-        <h2 className="sidebar-filters-text">Filter by Source</h2>
+      <h2 className="sidebar-filters-text">Filter by Source</h2>
         <select value={selected}
         onChange={(e) => setSelected(e.target.value)}>
-            <option value="">-All Sources-</option>
-            {articles.map((article) => (
-            <option value={article.source.name}>{article.source.name}</option>
-            ))}
+          <option value="">-All Sources-</option>
+          {articles.map((article) => (
+          <option value={article.source.name}>{article.source.name}</option>
+          ))}
         </select>
     </div>
 
     <h2 className="sidebar-share-text">Share</h2>
-    <ul className="sidebar-share">
+      <ul className="sidebar-share">
         <li className="sidebar-share-item"><a href="/">Email</a></li>
         <li className="sidebar-share-item"><a href="/">Twitter</a></li>
         <li className="sidebar-share-item"><a href="/">Facebook</a></li>
-    </ul>
-    </div>
+      </ul>
+  </div>
 );
 };
 
