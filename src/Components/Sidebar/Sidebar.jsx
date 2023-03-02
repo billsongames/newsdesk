4681b3a7958e68fd164df7b5fea0df813a681bbd
Carl Beckley <carlbeckley@gmail.com>
2023-03-02 19:50:12 +0000

    state and function added to select by source on Sidebar component

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import "./sidebar.css"
 
 const Sidebar = (props) => {
-    const { reverseOrder } = props;
+    const { reverseOrder, articles, selected, setSelected } = props;
     const [newArticles, setNewArticles] = useState(true);
     const [oldArticles, setOldArticles] = useState(false);
 
@@ -21,11 +21,22 @@ const Sidebar = (props) => {
 
 return (
     <div className="sidebar-container">
-    <h2 className="sidebar-filters-text">Sort by Date</h2>
-    <div className="sidebar">
-    <button className="sidebar-button" onClick={newClickHandler} disabled={newArticles}>Most Recent</button>
-    <button className="sidebar-button" onClick={oldClickHandler} disabled={oldArticles}>Oldest</button>
-</div>
+        <h2 className="sidebar-filters-text">Sort by Date</h2>
+        <div className="sidebar">
+            <button className="sidebar-button" onClick={newClickHandler} disabled={newArticles}>Most Recent</button>
+            <button className="sidebar-button" onClick={oldClickHandler} disabled={oldArticles}>Oldest</button>
+        </div>
+
+    <div>
+        <h2 className="sidebar-filters-text">Filter by Source</h2>
+        <select value={selected}
+        onChange={(e) => setSelected(e.target.value)}>
+            <option value="">-All Sources-</option>
+            {articles.map((article) => (
+            <option value={article.source.name}>{article.source.name}</option>
+            ))}
+        </select>
+    </div>
 
     <h2 className="sidebar-share-text">Share</h2>
     <ul className="sidebar-share">
